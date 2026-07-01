fe71c2b4f4307f135ff66c77f2dfd0656cd72da5
Paryag Kakkar <paryagkakkar06@gmail.com>
2026-07-01 22:10:21 +0530

    Added Profile Module

--- ai/main.py
+++ ai/main.py
@@ -1,10 +1,21 @@
-name="paryag"
-age=19
-cgpa=6.5
-is_student=True
-print(name)
-print(age)
-print(cgpa)
-print(is_student)
+while True:
+    choice = input("Enter your choice: ")
 
+    if choice == "1":
+        print("Profile")
 
+    elif choice == "2":
+        print("Dataset Manager")
+
+    elif choice == "3":
+        print("Machine Learning")
+
+    elif choice == "4":
+        print("AI Chat")
+
+    elif choice == "5":
+        print("Exit")
+        break
+
+    else:
+        print("Invalid Choice")
