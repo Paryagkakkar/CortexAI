f0b090958d7b7f719a9ac90a777ba645febf113d
Paryag Kakkar <paryagkakkar06@gmail.com>
2026-07-01 22:26:15 +0530

    modules  done

--- ai/main.py
+++ ai/main.py
@@ -1,21 +1,3 @@
-while True:
-    choice = input("Enter your choice: ")
-
-    if choice == "1":
-        print("Profile")
-
-    elif choice == "2":
-        print("Dataset Manager")
-
-    elif choice == "3":
-        print("Machine Learning")
-
-    elif choice == "4":
-        print("AI Chat")
-
-    elif choice == "5":
-        print("Exit")
-        break
-
-    else:
-        print("Invalid Choice")
+import math_utils
+result=math_utils.add(10,20)
+print(result)
