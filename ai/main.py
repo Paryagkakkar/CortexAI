e84a277fa8bf29860903815615fae47228c88927
Paryag Kakkar <paryagkakkar06@gmail.com>
2026-07-02 22:46:19 +0530

    dictionaries  done

--- ai/main.py
+++ ai/main.py
@@ -1,6 +1,13 @@
-data = ["Paryag", 21, 8.6, True]
-data.append("Python")
-data.insert(2,"Java")
-data.remove("Python")
-data.pop()
-print(data)
+students = [
+    {
+        "name": "Paryag",
+        "cgpa": 8.6
+    },
+    {
+        "name": "Priyal",
+        "cgpa": 9.8
+    }
+]
+
+for student in students:
+    print(student["name"])
