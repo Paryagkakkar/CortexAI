bce249ab494d846331638262aba0318ba0fba64c
Paryag Kakkar <paryagkakkar06@gmail.com>
2026-07-03 22:11:10 +0530

    oops  done

---
+++ ai/class_demo.py
@@ -0,0 +1,12 @@
+class Student:
+
+    def __init__(self, name, cgpa):
+        self.name = name
+        self.cgpa = cgpa
+
+
+student1 = Student("Paryag", 6.5)
+student2 = Student("Rahul", 7.5)
+
+print(student1.name)
+print(student2.name)
