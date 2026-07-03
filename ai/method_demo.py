beb552b885b5e5d2535cd7aadb567f71401061e1
Paryag Kakkar <paryagkakkar06@gmail.com>
2026-07-03 22:41:42 +0530

    inheritance  done

--- ai/method_demo.py
+++ ai/method_demo.py
@@ -1,14 +1,21 @@
-class student:
-    def __init__(self,name,age,cgpa):
-        self.name=name
-        self.age=age
-        self.cgpa=cgpa
-class teacher(student):
-    pass
-    def __init__(self,salary):
-        super().__init__(name,age)
-        self.salary=salary
-teacher1=teacher("paryag",19,8.7,10000)
-print(teacher1)
-        
-        
+class Student:
+
+    def __init__(self, name, age, cgpa):
+        self.name = name
+        self.age = age
+        self.cgpa = cgpa
+
+
+class Teacher(Student):
+
+    def __init__(self, name, age, cgpa, salary):
+        super().__init__(name, age, cgpa)
+        self.salary = salary
+
+
+teacher1 = Teacher("Paryag", 19, 8.7, 10000)
+
+print(teacher1.name)
+print(teacher1.age)
+print(teacher1.cgpa)
+print(teacher1.salary)    
