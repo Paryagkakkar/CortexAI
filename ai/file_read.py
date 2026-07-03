7fc17a167d644850338e1dc8adb45f1325976503
Paryag Kakkar <paryagkakkar06@gmail.com>
2026-07-03 21:51:05 +0530

    file  done

---
+++ ai/file_read.py
@@ -0,0 +1,4 @@
+file=open("students.txt","r")
+for data in file:
+    print(data)
+file.close()
---
+++ ai/file_write.py
@@ -0,0 +1,9 @@
+file = open("students.txt", "w")
+
+file.write("Paryag\n")
+file.write("Rahul\n")
+file.write("Priyal\n")
+
+file.close()
+
+print("Data Saved")
