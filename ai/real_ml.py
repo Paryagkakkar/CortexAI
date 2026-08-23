df8ff52de5a8dd13151fe31157dd0fba7e84445a
Paryag Kakkar <paryagkakkar06@gmail.com>
2026-08-23 21:54:39 +0530

    more about ml is done

--- ai/real_ml.py
+++ ai/real_ml.py
@@ -7,27 +7,17 @@ from sklearn.linear_model import LinearRegression
 # Load dataset
 df = pd.read_csv("../datasets/job_data.csv")
 
-
-# Convert Job_Role into numbers
-df_encoded = pd.get_dummies(
-    df,
-    columns=["Job_Role"],
-    dtype=int
-)
+print(df.head())
 
 
-# Features and target
-X = df_encoded.drop("Salary_LPA", axis=1)
+# Select input and output
+X = df[["Experience"]]
+y = df["Salary"]
 
-y = df_encoded["Salary_LPA"]
 
-
-# Train/Test split
+# Split dataset into training and testing data
 X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y,
-    test_size=0.2,
-    random_state=42
+    X, y, test_size=0.2, random_state=42
 )
 
 
@@ -37,3 +27,19 @@ model = LinearRegression()
 
 # Train model
 model.fit(X_train, y_train)
+
+
+# Make predictions
+predictions = model.predict(X_test)
+
+
+# Show actual values
+print("Actual salaries:")
+print(y_test.values)
+
+print()
+
+
+# Show predicted values
+print("Predicted salaries:")
+print(predictions)
