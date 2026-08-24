cc3cb6bd4c12fede2630807b7fab41aae4834046
Paryag kakkar <paryagkakkar06@gmail.com>
2026-08-24 22:09:24 +0530

    more about ml is done

--- ai/real_ml.py
+++ ai/real_ml.py
@@ -2,44 +2,35 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
 
 
 # Load dataset
 df = pd.read_csv("../datasets/job_data.csv")
-
-print(df.head())
-
-
-# Select input and output
-X = df[["Experience"]]
-y = df["Salary"]
-
-
-# Split dataset into training and testing data
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+df_encoded=pd.get_dummies(
+    df,
+    columns=["Job_Role"],
+    dtype=int
 )
-
-
-# Create model
-model = LinearRegression()
-
-
-# Train model
-model.fit(X_train, y_train)
-
-
-# Make predictions
-predictions = model.predict(X_test)
-
-
-# Show actual values
+X=df_encoded.drop("Salary_LPA",axis=1)
+y=df_encoded["Salary_LPA"]
+X_train,X_test,y_train,y_test=train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+model=LinearRegression()
+model.fit(X_train,y_train)
+predictions=model.predict(X_test)
 print("Actual salaries:")
 print(y_test.values)
-
 print()
-
-
-# Show predicted values
-print("Predicted salaries:")
+print("Predicted  salaries:")
 print(predictions)
+mae=mean_absolute_error(
+    y_test,
+    predictions
+)
+print()
+print("Mean Absolute Error",mae)
